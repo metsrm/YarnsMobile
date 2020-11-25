b103bbbc3c0e03bf0c5c17ce8ae625be392f832c
Ross Meyer <metsrm@yahoo.com>
2020-11-25 14:59:12 -0500

    Style changes

--- YarnsMobile/wwwroot/js/site.js
+++ YarnsMobile/wwwroot/js/site.js
@@ -3,7 +3,7 @@
 	nav.onclick = function () {
 		var list = document.querySelector("div#navicon + ul");
 		list.style.display = "block";
-	}
+	}zz
 
 	var x = document.getElementById("icon");
 	x.onclick = function () {
