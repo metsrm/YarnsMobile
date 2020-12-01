3c4885f273df160682e8af345760bc4a87feaab9
Ross Meyer <metsrm@yahoo.com>
2020-12-01 18:24:37 -0500

    Changed the DbContext to IdentityDbContext

--- YarnsMobile/Database/YarnsMobileContext.cs
+++ YarnsMobile/Database/YarnsMobileContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +8,7 @@ using YarnsMobile.Models;
 
 namespace YarnsMobile.Database
 {
-    public class YarnsMobileContext : DbContext
+    public class YarnsMobileContext : IdentityDbContext
     {
         public YarnsMobileContext(DbContextOptions<YarnsMobileContext> options) : base(options) { }
 
