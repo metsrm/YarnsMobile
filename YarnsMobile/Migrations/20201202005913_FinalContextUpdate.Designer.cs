930cdf11e49690d192ad8fde626456b6fc51b96d
Ross Meyer <metsrm@yahoo.com>
2020-12-01 19:59:47 -0500

    Final context updates

--- YarnsMobile/Migrations/20201202005913_FinalContextUpdate.Designer.cs
+++ YarnsMobile/Migrations/20201202005913_FinalContextUpdate.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using YarnsMobile.Database;
 
 namespace YarnsMobile.Migrations
 {
     [DbContext(typeof(YarnsMobileContext))]
-    partial class YarnsMobileContextModelSnapshot : ModelSnapshot
+    [Migration("20201202005913_FinalContextUpdate")]
+    partial class FinalContextUpdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
